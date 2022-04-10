5772ef8540585a53450c75a25d46b392508612ad
LyThanhTien10 <tien.lythanh@hcmut.edu.vn>
2022-04-11 00:55:31 +0700

    update file csv

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ async function getAllData(batches){
 };
 
 function saveData(data, fileName){
-    fs.writeFile(`${__dirname}/TEMP/${fileName}.json`, JSON.stringify(data, null, 4), (err) => {
+    fs.writeFile(`${__dirname}/${fileName}.json`, JSON.stringify(data, null, 4), (err) => {
         if (err) {  console.error(err);  return; };
         console.log("File has been created");
     });
@@ -47,8 +47,8 @@ function saveData(data, fileName){
 
 async function main(){
     /* Create cityID array */
-    var cityIdArr = Array.from({length:4},(_,index) => index + 61);
-    var fileName = "MARK61-64";
+    var cityIdArr = Array.from({length:64},(_,index) => index + 1);
+    var fileName = "ĐIỂM THI THPTQG 2021";
 
     /* Array of number student of each city */
     capacityArr = await modules.findCapacityOfAllCity(cityIdArr);
@@ -56,7 +56,7 @@ async function main(){
     /* Join all studentID from all city to 1 array */
     var idArr = [];
     for (let i=0;i<capacityArr.length;i++){
-        idArr = idArr.concat(Array.from({length: capacityArr[i]%1000000}, (_,index)=> modules.createStudentID((i+61)*1000000 + index + 1)));
+        idArr = idArr.concat(Array.from({length: capacityArr[i]%1000000}, (_,index)=> modules.createStudentID((i+1)*1000000 + index + 1)));
     }
 
     /* Create batches of StudentID with size 1000 element each batch */
